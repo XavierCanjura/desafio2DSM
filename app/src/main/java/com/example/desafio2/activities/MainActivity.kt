2eda5270a7a3b7fb3c9a1158d9916205d3312e46
Rubidia Paz <rubigarcia190@gmail.com>
2023-04-04 13:02:24 -0600

    Inicio de sesion y registro

--- app/src/main/java/com/example/desafio2/activities/MainActivity.kt
+++ app/src/main/java/com/example/desafio2/activities/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.desafio2.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.desafio2.R
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
+
 }
