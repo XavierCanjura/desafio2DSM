17f14de5cff8ebde6d69b9e8035c12976dc70d45
XavierCanjura <36569554+XavierCanjura@users.noreply.github.com>
2023-04-04 12:59:09 -0600

    products view and function

---
+++ app/src/main/java/com/example/desafio2/adapters/ProductAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.desafio2.adapters
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import com.example.desafio2.R
+import com.example.desafio2.models.ProductModel
+
+class ProductAdapter(private var productList: List<ProductModel>): Adapter<ProductViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder { // Pinta los atributos
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return ProductViewHolder(layoutInflater.inflate(R.layout.item_product, parent, false))
+    }
+
+    // Recorre cada uno de los items de la lista y llama el render del viewHolder
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        val movie = productList[position]
+        holder.render(movie)
+    }
+
+    override fun getItemCount(): Int = productList.size
+
+    fun setProductList(newProductList: List<ProductModel>){
+        this.productList = newProductList
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/com/example/desafio2/views/ProductsFragment.kt
+++ app/src/main/java/com/example/desafio2/views/ProductsFragment.kt
@@ -5,38 +5,67 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.desafio2.R
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.desafio2.adapters.ProductAdapter
+import com.example.desafio2.databinding.FragmentProductsBinding
+import com.example.desafio2.models.ProductModel
+import com.example.desafio2.viewmodels.ProductViewModel
+import com.google.firebase.database.*
 
 class ProductsFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding:FragmentProductsBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var viewModel: ProductViewModel
+    private var productAdapter: ProductAdapter? = null
+    var products: MutableList<ProductModel> = ArrayList()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_products, container, false)
+        _binding = FragmentProductsBinding.inflate(inflater,container,false)
+        val view = binding.root
+
+        initRecyclerView()
+
+        viewModel = ViewModelProvider(this)[ProductViewModel::class.java]
+        viewModel.getProducts()
+        viewModel.observerProductLiveData().observe(viewLifecycleOwner, Observer { productList ->
+            productAdapter?.setProductList(productList)
+        })
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun initRecyclerView(){
+        productAdapter = ProductAdapter(products)
+        binding.rvProductsList.layoutManager = LinearLayoutManager(context)
+        binding.rvProductsList.adapter = productAdapter
     }
 
     companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProductsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        var database: FirebaseDatabase = FirebaseDatabase.getInstance()
+        var refProducts: DatabaseReference = database.getReference("products")
     }
 }
